5056cb8f62c0e57126532a1f7048432fefffa0c7
Pedro Lucas <pedrodevmob@gmail.com>
2022-07-17 09:07:24 -0300

    Criado o ENUM para os estados observaveis

---
+++ lib/src/app/shared/utils/status.dart
@@ -0,0 +1,25 @@
+///Legenda:
+///
+/// NAO_CARREGADO: Estado inicial, nenhuma requisição foi feita.
+///
+/// ERRO: Requisição retornou um erro.
+///
+/// CONCLUIDO: Requisição retornou resposta adequada.
+///
+/// VAZIO: Requisição bem sucedida, mas retornou dados vazios.
+///
+/// AGUARDANDO: Requisição em andamento.
+
+enum Status {
+  NAO_CARREGADO,
+  ERRO,
+  CONCLUIDO,
+  VAZIO,
+  AGUARDANDO,
+}
+
+extension StatusExt on Status {
+  static String msg = '';
+  set mensagem(String novaMsg) => msg = novaMsg;
+  String get mensagem => msg;
+}
