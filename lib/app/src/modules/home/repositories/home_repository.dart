07b4220629df28e480c347b2b09dd47843bdfff5
Pedro Lucas <pedrodevmob@gmail.com>
2022-07-16 22:51:19 -0300

    Criado repository, agora implementar no controller com MOBX

--- lib/app/src/modules/home/repositories/home_repository.dart
+++ lib/app/src/modules/home/repositories/home_repository.dart
@@ -1,3 +1,14 @@
+import 'package:conversor_de_moedas/app/src/models/response_data.dart';
+import 'package:dio/dio.dart';
+
 class HomeRepository{
-  
+  final Dio _dio;
+
+  HomeRepository(this._dio);
+
+   Future<ResponseData> getDadosMoedas() async {
+    var response = await _dio.get('https://api.hgbrasil.com/finance/stock_price?key=ed660d36&symbol=bidi4');
+    ResponseData responseData = ResponseData.fromJson(response.data);
+    return responseData;
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
+import 'package:conversor_de_moedas/app/src/modules/home/repositories/home_repository.dart';
 import 'package:conversor_de_moedas/app_module.dart';
 import 'package:conversor_de_moedas/app_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-
 void main() {
+  
   runApp(
     ModularApp(
       module: AppModule(), 
